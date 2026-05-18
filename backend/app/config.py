bf5774d46e0731e3fd30288dacdc42d8bfcde678
Abdul Ahad <adibutt457@gmail.com>
2026-05-18 14:42:03 +0500

    Added Database URL in env

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,7 +4,7 @@ import os
 class Config:
     SECRET_KEY = os.getenv("@newBike4945", "change-me")
     JWT_SECRET_KEY = os.getenv("@newBike4945", "change-me")
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/healwise")
+    MONGO_URI = os.getenv("MONGO_URI")
     # Path to the local model directory inside the backend
     ML_MODEL_DIR = os.getenv("ML_MODEL_DIR", "app/ml_model/clinicalbert_finetuned")
 
